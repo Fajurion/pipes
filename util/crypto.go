824faa51728359357d6d3e413408bc581c335a81
Julian <70802809+Unbreathable@users.noreply.github.com>
2023-06-11 12:37:43 +0200

    Upgraded to AES GCM

--- util/crypto.go
+++ util/crypto.go
@@ -1,46 +1,56 @@
 package util
 
 import (
-	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"
 	"errors"
 	"io"
 )
 
-// encrypt encrypts text with key using AES-256 (credit to https://stackoverflow.com/questions/18817336/encrypting-a-string-with-aes-and-base64 with slight modifications)
+// EncryptAES encrypts text with key using AES-256 GCM
 func EncryptAES(block cipher.Block, text []byte) ([]byte, error) {
 
-	b := base64.StdEncoding.EncodeToString(text)
-	ciphertext := make([]byte, aes.BlockSize+len(b))
-
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	cfb := cipher.NewCFBEncrypter(block, iv)
-	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(b))
-	return ciphertext, nil
-}
-
-// decrypt decryptes text with key using AES-256 (credit to https://stackoverflow.com/questions/18817336/encrypting-a-string-with-aes-and-base64 with slight modifications)
-func DecryptAES(block cipher.Block, text []byte) ([]byte, error) {
-
-	if len(text) < aes.BlockSize {
-		return nil, errors.New("ciphertext too short")
-	}
-
-	iv := text[:aes.BlockSize]
-	text = text[aes.BlockSize:]
-
-	cfb := cipher.NewCFBDecrypter(block, iv)
-	cfb.XORKeyStream(text, text)
-	data, err := base64.StdEncoding.DecodeString(string(text))
+	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
 
-	return data, nil
+	// Get nonce
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	// Encrypt the plaintext using the GCM cipher
+	text = gcm.Seal(nil, nonce, text, nil)
+
+	// Append the nonce to the ciphertext
+	text = append(nonce, text...)
+
+	return text, nil
+}
+
+// DecryptAES decryptes text with key using AES-256 GCM
+func DecryptAES(block cipher.Block, text []byte) ([]byte, error) {
+
+	// Get GCM cipher
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ensure the ciphertext is of the minimum length
+	if len(text) < gcm.NonceSize() {
+		return nil, errors.New("ciphertext is too short")
+	}
+
+	// Get nonce
+	nonce := text[:gcm.NonceSize()]
+
+	plaintext, err := gcm.Open(nil, nonce, text[gcm.NonceSize():], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
 }
